25c2bdb2a9e1b8484b2a6424611a50680f585604
FilipKorev <filipkorev95@gmail.com>
2022-11-17 20:31:36 +0100

    if

--- JavaScript/01_GettingStartedWithJavaScript/ex 1.5/main.js
+++ JavaScript/01_GettingStartedWithJavaScript/ex 1.5/main.js
@@ -1,7 +1,24 @@
-console.log("Filip Korev");
+// console.log("Filip Korev");
 /*
-sfasg
-adsgadas
-dsgdsgs
+multi
+line 
+comment
 */
-prompt("");
+/*
+let username = prompt("insert your name!");
+alert("vaseto ime " + username + " e procesirano!")
+console.log(username);
+*/
+
+
+let randomNum = Math.random() * 3;
+console.log("my random number " + randomNum);
+let roundNum = Math.floor(randomNum) +1;
+console.log("my round number " + roundNum);
+let num = prompt("Guess a number from 1 to 3")
+
+if (roundNum == num) {
+    alert("you guessed right!");
+} else {
+    alert("You guessed wrong!");
+}
