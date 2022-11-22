47c539bb2b607c6b1b6b5d13ad6e7a8370b2468f
FilipKorev <filipkorev95@gmail.com>
2022-11-22 20:49:22 +0100

    2.2

---
+++ JavaScript/02_JavaScriptEssentials/ex2.2/main.js
@@ -0,0 +1,11 @@
+let name = 'filip';
+let age = 25;
+let canCode = true;
+
+console.log("Hello, my name is " + name + " i am " + age + 
+" years old and i can code JavaScript: " + canCode);
+
+console.log(`Hello, my name is ${name} i am ${age} years old and i 
+can code JavaScript: ${canCode}`);
+
+
