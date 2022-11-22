16a3a9f016b970d48c5b7978e4f7c58926e5fd47
FilipKorev <filipkorev95@gmail.com>
2022-11-22 19:46:40 +0100

    ex2.1

---
+++ excercise/19.11.2022/main.js
@@ -0,0 +1,16 @@
+let lenght = 16;                             //number
+let lastName = "Johnson";                    //string
+let x = {firstName:"John", lastName:"Doe"};  //Object
+
+console.log(lenght);
+console.log(lastName);
+console.log(x);
+
+let a = 10;
+let b = 15;
+
+let c = a + b;
+
+console.log(c);
+
+console.log("the sum of " + a + " and " + b + " is: " + (a + b));
