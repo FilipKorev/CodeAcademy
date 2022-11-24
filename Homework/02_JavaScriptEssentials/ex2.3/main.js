e540ee396c80dec3d251dc2661097eb90ede358a
FilipKorev <filipkorev95@gmail.com>
2022-11-24 12:58:19 +0100

    2.3

---
+++ Homework/02_JavaScriptEssentials/ex2.3/main.js
@@ -0,0 +1,13 @@
+a =  prompt ("a = ?");
+console.log("a = " + a);
+b = prompt ("b = ?");
+console.log("b = " + b);
+a2 = a**2;
+console.log("a2 = " + a2);
+b2 = b**2;
+console.log("b2 = " + b2);
+c2 = a2 + b2;
+console.log("c2 = " + c2);
+c = Math. sqrt(c2);
+console.log("c = " + c);
+
