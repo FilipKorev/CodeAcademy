51250be74c5b5ee342143de5ffa8d3498e3a1cc7
FilipKorev <filipkorev95@gmail.com>
2022-11-24 13:40:21 +0100

    2.4

---
+++ Homework/02_JavaScriptEssentials/ex2.4/main.js
@@ -0,0 +1,13 @@
+a = 5;
+console.log("a = " + a);
+b = 10;
+console.log("b = " + b);
+c = 12;
+console.log("c = " + c);
+a = a + b;
+console.log("a+b = " + a);
+a = a / c;
+c = c % b;
+console.log("a/c = " + a);
+console.log("b = " + b);
+console.log("c%b = " + c);
