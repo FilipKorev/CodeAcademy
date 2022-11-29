2d0e7425c307953e6d4c2fa1816ad9169fb9a8b3
FilipKorev <filipkorev95@gmail.com>
2022-11-29 12:49:03 +0100

    homework 1.9

---
+++ Homework/02_JavaScriptEssentials/ex/main.js
@@ -0,0 +1,45 @@
+//ex1.1
+let firstname = Lata;
+
+//1.2
+let x = 'Greeta'; //x has a value of 'Greeta'
+
+//1.3
+let flower = 'rose';
+let tree = 'maple';
+
+//1.4
+let x = 'tic';
+x = 'tac';
+x = 'toe';  //x has a value of 'toe'
+
+//1.5
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;  //x has a value of 'Hardy'
+
+//1.6
+// -9*3= -27
+//"value is " + 50= "value is 50"
+//17 % 5 = 2
+//5 % 17 = 5
+// 5/10 = 0.5
+//(4==4) = true
+//(4!=5) = true
+//(7<=8) = true
+//Math.ceil(x) - Math.floor(x) = for any number on x it is 0
+
+//1.7
+alert("Hello world");
+
+//1.8
+let num1 = prompt("type any number");
+alert(num1);
+
+//1.9
+let n1 = 5;
+let n2 = 10;
+console.log(`The product of ${n1} and ${n2} is ${n1 * n2}`);
+
