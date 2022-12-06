b605bbae08f77f71f59958b36fea2a12dba7c3f2
FilipKorev <filipkorev95@gmail.com>
2022-12-06 12:55:20 +0100

    ex1

---
+++ JavaScript/exercises/03.11.2022/ex1/main.js
@@ -0,0 +1,16 @@
+let arr = ["str1", "str2","str3", "str4","str5"];
+console.log(arr[0]);
+console.log(arr.indexOf("str4"));
+//?
+lastArr = arr.length - 1
+console.log(lastArr);
+arr.sort();
+arr.reverse();
+//random?
+arr.push("str6");
+
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let min = Math.min();
+console.log(min);
+
