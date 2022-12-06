878e59b63dcde527491ce770dcf05f5ef1dffd5e
FilipKorev <filipkorev95@gmail.com>
2022-12-06 18:35:45 +0100

    ex4.1

---
+++ JavaScript/04_LogicStatements/ex4.1/main.js
@@ -0,0 +1,18 @@
+let var1 = true;
+console.log(var1);
+if(var1) {
+    console.log("it's true");
+}
+
+if(!var1){
+    console.log("it's not true");
+}
+
+var1 = false;
+console.log(var1);
+if(var1) {
+    console.log("it's true");
+}
+if(!var1){
+    console.log("it's not true");
+}
