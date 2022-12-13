1946e3b07f7ec81c414b72d7c897468c9269e582
FilipKorev <filipkorev95@gmail.com>
2022-12-13 18:59:10 +0100

    update

--- JavaScript/04_LogicStatements/numberGame/main.js
+++ JavaScript/04_LogicStatements/numberGame/main.js
@@ -6,6 +6,12 @@ if(userNum > dynNum) {
     console.log("your number is equal to " + dynNum);
 } else if(userNum < dynNum){
     console.log("you number is less than " + dynNum);
-} else {
+}else {
     console.log("NaN");
 }
+
+let messege = userNum > dynNum ?
+"your number is greater than " : userNum === dynNum ?
+"your number is equal to " : userNum < dynNum ?
+"you number is less than " : "NaN" ;
+console.log(messege + dynNum);
