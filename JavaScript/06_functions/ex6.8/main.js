180b1e6c4e17c63c187bf3e223bd97b3fe7f4145
FilipKorev <filipkorev95@gmail.com>
2023-01-26 19:26:01 +0100

    chas

--- JavaScript/06_functions/ex6.8/main.js
+++ JavaScript/06_functions/ex6.8/main.js
@@ -9,3 +9,16 @@ function variable2(x) {
 
 variable2(5)
 
+//povikuvanje na funkcija vo funkcija
+function doFlexibleStuff(executeStaff) {
+    executeStaff(15);
+    console.log("insude doFlexibleStuff");
+}
+doFlexibleStuff(variable);
+
+let logName = function () {
+    console.log("My name is Filip");
+}
+
+
+setTimeout(logName, 10000)
